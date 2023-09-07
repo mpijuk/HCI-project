25582e56f475f8412656d86412464970cee33eba
Mario Pijuk <mpijuk00@fesb.hr>
2023-09-07 21:28:02 +0200

    Fixed react icons import 2

--- component/Header.js
+++ component/Header.js
@@ -1,6 +1,6 @@
 import styles from "../styles/Header.module.css";
 import {PiShoppingCartLight, PiCookingPot} from "react-icons/pi";
-import {GoPerson} from "react-icons/Go";
+import {GoPerson} from "react-icons/goo";
 import Navbar from "./Navbar";
 import Link from "next/link";
 import { useContext } from "react";
